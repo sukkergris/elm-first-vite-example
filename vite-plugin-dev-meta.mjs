4e3e3b8902667900a334c8fd49b4d54ffb19476c
Theodor Heiselberg <theodor.heiselberg@stil.dk>
2025-12-30 13:12:21 +0100

    Wrong usage of vite param --mode

--- vite-plugin-dev-meta.mjs
+++ vite-plugin-dev-meta.mjs
@@ -1,13 +1,13 @@
-export default function devMetaTagPlugin(command, mode) {
-  if (command !== 'serve') return null;
+export default function devMetaTagPlugin(command) {
+  if (command !== "serve") return null;
 
   return {
-    name: 'html-transform-dev-only',
+    name: "html-transform-dev-only",
     transformIndexHtml(html) {
       return html.replace(
-        '</head>',
-        `<meta name="environment-name" content="${mode}"></head>`
+        "</head>",
+        `<meta name="environment-name" content="local"></head>`
       );
-    }
+    },
   };
 }
--- vite.config.mjs
+++ vite.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 import elm from 'vite-plugin-elm-watch';
 import devMetaTagPlugin from './vite-plugin-dev-meta.mjs';
 
-export default defineConfig(({ command, mode }) => ({
+export default defineConfig(({ command }) => ({
   publicDir: "public",
 
   build: {
@@ -11,22 +11,18 @@ export default defineConfig(({ command, mode }) => ({
     emptyOutDir: true,
   },
 
-  plugins: [
-    elm(),
-    devMetaTagPlugin(command, mode),
-    tailwindcss()
-  ],
+  plugins: [elm(), devMetaTagPlugin(command), tailwindcss()],
 
   server: {
-    open: 'http://happy.now',
+    open: "http://happy.now",
     port: 3456,
     host: "0.0.0.0", // Listen on all network interfaces to allow access from the host machine
-    allowedHosts: ["host.docker.internal", "www.happy.now", "happy.now", "www.happy.dev", "happy.dev"],
+    allowedHosts: ["host.docker.internal", ".happy.now", ".happy.dev"],
   },
   preview: {
-    host: '0.0.0.0',
+    host: "0.0.0.0",
     port: 3456,
     strictPort: true,
-    allowedHosts: ["www.happy.dev", "happy.dev"]
+    allowedHosts: [".happy.dev"],
   },
 }));
